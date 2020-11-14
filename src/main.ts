d4c8c55f52f1dd89b79c03f6f508c05289bd4ec5
Paul Evans <paul3vanz@gmail.com>
2020-11-14 22:04:40 +0000

    Add amplify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import Amplify from 'aws-amplify';
 import { AppModule } from './app/app.module';
+import aws_exports from "./aws-exports";
+import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +11,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+Amplify.configure(aws_exports);
+  
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -2,6 +2,10 @@
  * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
  */
 import '@angular/localize/init';
+/***************************************************************************************************
+ * Zone JS is required by default for Angular itself.
+ */
+import 'zone.js/dist/zone';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -56,12 +60,14 @@ import '@angular/localize/init';
  *
  */
 
-/***************************************************************************************************
- * Zone JS is required by default for Angular itself.
- */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
+ // Included with Angular CLI.
 
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+(window as any).global = window;
+(window as any).process = {
+  env: { DEBUG: undefined },
+};
